8f793db44c0684668a3ff281eeccb8a3e308593f
Alejandro Gomez <jagomez967@hotmail.com>
2018-05-20 20:52:10 -0300

    Correcciones simples ejercicio 2

--- ej01_par_o_impar.kt
+++ ej01_par_o_impar.kt
@@ -3,23 +3,20 @@ fun main(args: Array<String>){
 
 	if(args.size == 0){
 		println("No hay parametros de entrada")
-	}else{
-		var numero: Int? = args[0].toIntOrNull()
-		
-		if(numero == null){
-			println("La entrada '" + args[0] + "' tiene que ser un numero")
-			return
-		}
-		
-		var resto = numero % 2;
-		
-		if( resto == 0){
-			println("El numero '" + numero + "' es PAR")
-		}else{ 
-			println("El numero '" + numero + "' es IMPAR")
-		}
-		
-		
+		return
+	}
+	var numero: Int? = args[0].toIntOrNull()
+	if(numero == null){
+		println("La entrada '" + args[0] + "' tiene que ser un numero")
+		return
 	}
 
+	var resto = numero % 2
+
+	if( resto == 0){
+		println("El numero '" + numero + "' es PAR")
+		}
+	else{
+		println("El numero '" + numero + "' es IMPAR")
+	}
 }
