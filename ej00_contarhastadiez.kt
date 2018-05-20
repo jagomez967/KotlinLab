6b584f16630ace5cb80be1f62de50c7e36b39b25
Alejandro Gomez <jagomez967@hotmail.com>
2018-05-20 17:24:47 -0300

    Commiteando desde ATOM

--- ej00_contarhastadiez.kt
+++ ej00_contarhastadiez.kt
@@ -6,18 +6,18 @@ fun main(args: Array<String>){
 		println("No hay parametros de entrada")
 	}else{
 		var tope: Int? = args[0].toIntOrNull()
-		
+
 		if(tope == null){
 			tope = 10
 		}
-		
+
 		println("Voy a contar hasta " + tope + "...")
-		
+
 		for( i in 0..tope){
 			println(i)
 		}
-		
-		
-	}
 
+
+	}
+//ningun cambio en especial
 }
