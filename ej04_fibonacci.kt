89709b0012b5de9a78183ba1af448e1271eba685
Alejandro Gomez <jagomez967@hotmail.com>
2018-05-20 22:04:11 -0300

    Ejercicio fibonacci Refactorizado

--- ej04_fibonacci.kt
+++ ej04_fibonacci.kt
@@ -1,29 +1,25 @@
+fun _fibonacci(anterior: Int,actual: Int,corte: Int){
+	var nuevo: Int = anterior + actual
+	println(actual)
+	if(corte > nuevo)
+		return _fibonacci(actual,nuevo,corte)
+}
+
+fun fibonacci(maximo: Int){
+	 _fibonacci(0,1,maximo)
+}
 
 fun main(args: Array<String>){
 
 	if(args.size == 0){
 		println("No hay parametros de entrada")
-	}else{
-		var numero: Int? = args[0].toIntOrNull()
-		
-		if(numero == null){
-			println("La entrada '" + args[0] + "' tiene que ser un numero")
-			return
-		}
-		
-		if(numero == 0){
-			println("El numero es: 0")
-		}else if(numero == 1){
-			println("El numero es: 1")
-		}else{
-			var elMasAnterior = 0
-			var elAnterior = 1
-			for( i in 2..numero){
-				var nuevoValor = elMasAnterior + elAnterior
-				elMasAnterior = elAnterior
-				elAnterior = nuevoValor
-			}
-			println("El numero es:" + elAnterior)
-		}
+		return
 	}
+	var numero: Int? = args[0].toIntOrNull()
+
+	if(numero == null){
+		println("La entrada '" + args[0] + "' tiene que ser un numero")
+		return
+	}
+	fibonacci(numero)
 }
