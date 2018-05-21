37047e061602c592deb90c8cdf55ea0fa08d45ae
Alejandro Gomez <jagomez967@hotmail.com>
2018-05-20 22:03:43 -0300

    Ejercicio Vuelto refactorizado.

--- ej03_vuelto.kt
+++ ej03_vuelto.kt
@@ -5,45 +5,43 @@ fun main(args: Array<String>){
 		println("No hay parametros de entrada")
 	}else{
 		var numero: Int? = args[0].toIntOrNull()
-		
+
 		if(numero == null){
 			println("La entrada '" + args[0] + "' tiene que ser un numero")
 			return
 		}
-		
-		if (numero >=100){ 
+
+		if (numero >=100){
 			println("Billetes de 100 x " + (numero / 100) )
 		}
 		numero = numero % 100
-		
-		if (numero >=50){ 
+
+		if (numero >=50){
 			println("Billetes de 50 x " + (numero / 50) )
 		}
 		numero = numero % 50
-		
-		if (numero >=20){ 
+
+		if (numero >=20){
 			println("Billetes de 20 x " + (numero / 20) )
 		}
 		numero = numero % 20
-		if (numero >=10){ 
+		if (numero >=10){
 			println("Billetes de 10 x " + (numero / 10) )
 		}
 		numero = numero % 10
-		if (numero >=5){ 
+		if (numero >=5){
 			println("Billetes de 5 x " + (numero / 5) )
 		}
 		numero = numero % 5
-		
-		if (numero >=2){ 
+
+		if (numero >=2){
 			println("Billetes de 2 x " + (numero / 2) )
 		}
 		numero = numero % 2
-		
-		if (numero >=1){ 
+
+		if (numero >=1){
 			println("Billetes de 1 x " + (numero ) )
 		}
-		
-		
 	}
 
 }
--- ej03_vuelto_bis1.kt
+++ ej03_vuelto_bis1.kt
@@ -1,36 +1,26 @@
 
 fun decimeCuantosBilletesDe(vuelto: Int, denominacion: Int) : Int{
-	if (vuelto >=denominacion){ 
+	if (vuelto >=denominacion){
 		println("Billetes de " + denominacion + " x " + (vuelto / denominacion) )
 	}
-	
 	return vuelto % denominacion;
 }
 
 fun main(args: Array<String>){
-
 	if(args.size == 0){
 		println("No hay parametros de entrada")
-	}else{
-		var numero: Int? = args[0].toIntOrNull()
-		
-		if(numero == null){
-			println("La entrada '" + args[0] + "' tiene que ser un numero")
-			return
-		}
-		
-		
-		
-		numero = decimeCuantosBilletesDe(numero, 100)
-		numero = decimeCuantosBilletesDe(numero, 50)
-		numero = decimeCuantosBilletesDe(numero, 20)
-		numero = decimeCuantosBilletesDe(numero, 10)
-		numero = decimeCuantosBilletesDe(numero, 5)
-		numero = decimeCuantosBilletesDe(numero, 2)
-		decimeCuantosBilletesDe(numero, 1)
-
-		
-		
+		return
 	}
 
+	var numero: Int? = args[0].toIntOrNull()
+
+	if(numero == null){
+		println("La entrada '" + args[0] + "' tiene que ser un numero")
+		return
+	}
+	val listaBilletes: IntArray = intArrayOf(100, 50, 20, 10, 5, 2 , 1)
+
+	for(billete in listaBilletes)
+		if(numero != null)
+			numero = decimeCuantosBilletesDe(numero,billete)
 }
