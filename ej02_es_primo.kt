e3dd3436591c1309a98662cf24c81ec93129881b
Alejandro Gomez <jagomez967@hotmail.com>
2018-05-20 22:03:56 -0300

    Ejercicio Es primo refactorizado

--- ej02_es_primo.kt
+++ ej02_es_primo.kt
@@ -3,24 +3,23 @@ fun main(args: Array<String>){
 
 	if(args.size == 0){
 		println("No hay parametros de entrada")
-	}else{
-		var numero: Int? = args[0].toIntOrNull()
-		
-		if(numero == null){
-			println("La entrada '" + args[0] + "' tiene que ser un numero")
-			return
-		}
-		
-		var anterior = (numero - 1)
-		
-		for (i in 2..anterior){
-			if(numero % i ==0){
-				println("EL numero " + numero +  " no es primo")
-				return
-			}
-			
-		}
-		println("El numero "+ numero + " es primo")
+		return
 	}
 
+	var numero: Int? = args[0].toIntOrNull()
+
+	if(numero == null){
+		println("La entrada '" + args[0] + "' tiene que ser un numero")
+		return
+	}
+
+	var anterior = (numero - 1)
+
+	for (i in 2..anterior){
+		if(numero % i ==0){
+			println("EL numero " + numero +  " no es primo")
+			return
+		}
+	}
+	println("El numero "+ numero + " es primo")
 }
